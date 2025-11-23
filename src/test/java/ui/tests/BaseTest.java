0ec3bc8e35275e63fd1381d33911867e121f977e
Danil Chex <danilchex01@mail.com>
2025-11-23 12:03:47 +0300

    Добавил зависимости и код для тестов(пупкин)

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -1,4 +1,10 @@
 package ui.tests;
 
+import org.junit.jupiter.api.Test;
+
 public class BaseTest {
+    @Test
+    public void testMethod() {
+        System.out.println("Проверка связипше ");
+    }
 }
