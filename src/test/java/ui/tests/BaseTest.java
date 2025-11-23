1050c17f1311ab1384c36ab6684086c4e5444e91
Danil Chex <danilchex01@mail.com>
2025-11-23 13:26:08 +0300

    Комичу типо я иванов с простыми изменениями

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -6,5 +6,6 @@ public class BaseTest {
     @Test
     public void testMethod() {
         System.out.println("Проверка связипше ");
+        System.out.println("Я пишу ебаный код, типо я иванов");
     }
 }
